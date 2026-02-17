688b8021deba88470a8c8e221a883b5797a7223d
Tom Wilson <thomas.wilson@launchfirestorm.com>
2026-02-16 16:18:28 -0800

    migrate nvim LSP from lsp-zero to native 0.11 API
    Removed deprecated lsp-zero plugin and migrated to Neovim 0.11's native vim.lsp.config/enable API. Fixes recurring _watchfiles module errors after sleep/long sessions.

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -1,31 +1,38 @@
-local lsp = require("lsp-zero")
-
-lsp.preset("recommended")
-
+-- Native Neovim 0.11+ LSP setup (lsp-zero is deprecated)
 require('mason').setup({})
 require('mason-lspconfig').setup({
     ensure_installed = {
         'lua_ls',
-	'rust_analyzer',
-	'clangd',
-	'pylsp',
-	'texlab',
+        'rust_analyzer',
+        'clangd',
+        'pyright',
+        'texlab',
         'cmake',
         'glsl_analyzer',
         'wgsl_analyzer',
         'autotools_ls',
-        'typst_lsp'
-        -- 'mlir-lsp-server',
-        -- 'tblgen-lsp-server'
     },
     handlers = {
-        lsp.default_setup
+        function(server_name)
+            vim.lsp.enable(server_name)
+        end,
+        -- Custom clangd configuration
+        clangd = function()
+            vim.lsp.config('clangd', {
+                cmd = {
+                    'clangd',
+                    '--query-driver=/usr/bin/g++',
+                    '--background-index',
+                    '--clang-tidy',
+                },
+            })
+            vim.lsp.enable('clangd')
+        end,
     },
 })
 
 
 local cmp = require('cmp')
-local cmp_action = lsp.cmp_action()
 local cmp_select = {behavior = cmp.SelectBehavior.Select}
 cmp.setup({
     window = {
@@ -33,12 +40,18 @@ cmp.setup({
         documentation = cmp.config.window.bordered(),
     },
     mapping = cmp.mapping.preset.insert({
-      ['<Tab>'] = cmp.mapping.select_prev_item(cmp_select),
-      ['<S-Tab>'] = cmp.mapping.select_next_item(cmp_select),
+      ['<Tab>'] = cmp.mapping.select_next_item(cmp_select),
+      ['<S-Tab>'] = cmp.mapping.select_prev_item(cmp_select),
       ['<Enter>'] = cmp.mapping.confirm({ select = true }),
       ["<C-Space>"] = cmp.mapping.complete(),
-      ['<C-f>'] = cmp_action.luasnip_jump_forward(),
-      ['<C-b>'] = cmp_action.luasnip_jump_backward(),
+      ['<C-f>'] = cmp.mapping(function() require('luasnip').jump(1) end, {'i', 's'}),
+      ['<C-b>'] = cmp.mapping(function() require('luasnip').jump(-1) end, {'i', 's'}),
+    }),
+    sources = cmp.config.sources({
+        { name = "nvim_lsp" },
+        { name = "luasnip" },
+    }, {
+        { name = "buffer"},
     }),
     snippet = {
         expand = function(args)
@@ -48,42 +61,35 @@ cmp.setup({
 })
 
 
--- lsp.setup_nvim_cmp({
---   mapping = cmp_mappings
--- })
+-- LSP keybindings on attach
+vim.api.nvim_create_autocmd('LspAttach', {
+  callback = function(args)
+    local client = vim.lsp.get_client_by_id(args.data.client_id)
+    local bufnr = args.buf
+    local opts = {buffer = bufnr, remap = false}
 
-lsp.set_preferences({
-    suggest_lsp_servers = false,
-    sign_icons = {
-        error = 'E',
-        warn = 'W',
-        hint = 'H',
-        info = 'I'
-    }
+    if client.name == "eslint" then
+        vim.lsp.stop_client(client.id)
+        return
+    end
+
+    vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+    vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+    vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+    vim.keymap.set("n", "<leader>vws", vim.lsp.buf.workspace_symbol, opts)
+    vim.keymap.set("n", "<leader>vd", vim.diagnostic.open_float, opts)
+    vim.keymap.set("n", "[d", vim.diagnostic.goto_next, opts)
+    vim.keymap.set("n", "]d", vim.diagnostic.goto_prev, opts)
+    vim.keymap.set("n", "<leader>vca", vim.lsp.buf.code_action, opts)
+    vim.keymap.set("n", "<leader>vrr", vim.lsp.buf.references, opts)
+    vim.keymap.set("n", "<leader>vrn", vim.lsp.buf.rename, opts)
+    vim.keymap.set("i", "<C-h>", vim.lsp.buf.signature_help, opts)
+  end,
 })
 
-lsp.on_attach(function(client, bufnr)
-  local opts = {buffer = bufnr, remap = false}
-
-  if client.name == "eslint" then
-      vim.cmd.LspStop('eslint')
-      return
-  end
-
-  vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-  vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-  vim.keymap.set("n", "<leader>vws", vim.lsp.buf.workspace_symbol, opts)
-  vim.keymap.set("n", "<leader>vd", vim.diagnostic.open_float, opts)
-  vim.keymap.set("n", "[d", vim.diagnostic.goto_next, opts)
-  vim.keymap.set("n", "]d", vim.diagnostic.goto_prev, opts)
-  vim.keymap.set("n", "<leader>vca", vim.lsp.buf.code_action, opts)
-  vim.keymap.set("n", "<leader>vrr", vim.lsp.buf.references, opts)
-  vim.keymap.set("n", "<leader>vrn", vim.lsp.buf.rename, opts)
-  vim.keymap.set("i", "<C-h>", vim.lsp.buf.signature_help, opts)
-end)
-
-lsp.setup()
-
-vim.diagnostic.config({
-    virtual_text = true,
+-- Show diagnostic when hovering cursor
+vim.api.nvim_create_autocmd("CursorHold", {
+  callback = function()
+    vim.diagnostic.open_float(nil, { focusable = false })
+  end,
 })
--- nvim/lua/tw-ilson/lazy.lua
+++ nvim/lua/tw-ilson/lazy.lua
@@ -1,114 +1,119 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
 end
-vim.opt.rtp:prepend(lazypath)  
+vim.opt.rtp:prepend(lazypath)
 return require('lazy').setup({
-    "williamboman/mason.nvim",
-    "williamboman/mason-lspconfig.nvim",
-    "neovim/nvim-lspconfig",
-   {
-	  'numToStr/Comment.nvim',-- "gc" to comment visual regions/lines
-	  config = function ()
-		  require('Comment').setup()
-	  end
-  },
-  -- icons
-   'nvim-tree/nvim-web-devicons',
-  -- UI to select things (files, grep results, open buffers...)
-   {
-	'nvim-neo-tree/neo-tree.nvim',
-	dependencies = {
-	      "nvim-lua/plenary.nvim",
-	      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-	      "MunifTanjim/nui.nvim"
-	}
-  },
-
-   { 'nvim-telescope/telescope.nvim',
-	  dependencies = { 'nvim-lua/plenary.nvim', 'BurntSushi/ripgrep', 'sharkdp/fd' },
-  },
-  -- {
-  --   "nvim-telescope/telescope-file-browser.nvim",
-  --   dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" }
-  -- },
-   {
-	  'nvim-lualine/lualine.nvim', -- Fancier statusline
-	  dependencies = {'nvim-tree/nvim-web-devicons'}
-  },
-  -- Add indentation guides even on blank lines
-   { 'lewis6991/gitsigns.nvim', dependencies = { 'nvim-lua/plenary.nvim' } },
-  -- Highlight, edit, and navigate code using a fast incremental parsing library
-   'nvim-treesitter/nvim-treesitter',
-  -- Smooth scrolling
-   'psliwka/vim-smoothie',
-  -- Auto Save
-   {
-	"Pocco81/auto-save.nvim",
-	config = function()
-       require("auto-save").setup {
-        -- your config goes here
-       }
-    end,
-  },
-   {
-    "aserowy/tmux.nvim",
-  },
-   {'romgrk/barbar.nvim', dependencies = 'nvim-web-devicons'},
-   {
-	  'VonHeikemen/lsp-zero.nvim',
-	  dependencies = {
-		  -- LSP Support
-		  'neovim/nvim-lspconfig',
-		  'williamboman/mason.nvim',
-		  'williamboman/mason-lspconfig.nvim',
-
-		  -- Autocompletion
-		  'hrsh7th/nvim-cmp',
-		  'hrsh7th/cmp-buffer',
-		  'hrsh7th/cmp-path',
-		  'saadparwaiz1/cmp_luasnip',
-		  'hrsh7th/cmp-nvim-lsp',
-		  'hrsh7th/cmp-nvim-lua',
-
-		  -- Snippets
-		  'L3MON4D3/LuaSnip',
-		  'rafamadriz/friendly-snippets',
-	  }
-  },
-  {
-      'mrcjkb/rustaceanvim',
-      version = '^4', -- Recommended
-      lazy = false, -- This plugin is already lazy
-  },
-  --  {
-  --   'nvim-neorg/neorg',
-  --   config = function()
-  --       require('neorg').setup {
-  --           load = {
-  --               ["core.defaults"] = {}, -- Loads default behaviour
-  --               ["core.norg.concealer"] = {}, -- Adds pretty icons to your documents
-  --               ["core.norg.dirman"] = { -- Manages Neorg workspaces
-  --                   config = {
-  --                       workspaces = {
-  --                           notes = "~/Sync/notes",
-  --                       },
-  --                   },
-  --               },
-  --           },
-  --       }
-  --   end,
-  --   run = ":Neorg sync-parsers",
-  --   dependencies = "nvim-lua/plenary.nvim",
-  -- },
-  --Math in Ascii
-   'jbyuki/nabla.nvim',
-   'sbdchd/neoformat',
+    {
+        'nvim-neo-tree/neo-tree.nvim',
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+            "MunifTanjim/nui.nvim"
+        }
+    },
+    {
+        'nvim-telescope/telescope.nvim',
+        dependencies = { 'nvim-lua/plenary.nvim', 'BurntSushi/ripgrep', 'sharkdp/fd', 'nvim-telescope/telescope-live-grep-args.nvim' },
+        config = function()
+            local telescope = require("telescope")
+            telescope.setup({})
+            telescope.load_extension("live_grep_args")
+        end
+    },
+    {
+        'nvim-lualine/lualine.nvim', -- Fancier statusline
+        dependencies = { 'nvim-tree/nvim-web-devicons' }
+    },
+    -- Add indentation guides even on blank lines
+    { 'lewis6991/gitsigns.nvim', dependencies = { 'nvim-lua/plenary.nvim' }, config = function() require("gitsigns")
+            .setup({}) end },
+    -- Highlight, edit, and navigate code using a fast incremental parsing library
+    {
+        'nvim-treesitter/nvim-treesitter', opts = { ensure_installed = { "cpp" } }
+    },
+    -- Smooth scrolling
+    'psliwka/vim-smoothie',
+    -- fancy status bar
+    { 'romgrk/barbar.nvim',      dependencies = 'nvim-web-devicons',         config = function() require("barbar").setup({}) end },
+    -- LSP (native Neovim 0.11+ setup, lsp-zero is deprecated)
+    {
+        'neovim/nvim-lspconfig',
+        dependencies = {
+            'williamboman/mason.nvim',
+            'williamboman/mason-lspconfig.nvim',
+        }
+    },
+    -- Autocompletion
+    {
+        'hrsh7th/nvim-cmp',
+        dependencies = {
+            'hrsh7th/cmp-buffer',
+            'hrsh7th/cmp-path',
+            'saadparwaiz1/cmp_luasnip',
+            'hrsh7th/cmp-nvim-lsp',
+            'hrsh7th/cmp-nvim-lua',
+            'L3MON4D3/LuaSnip',
+            'rafamadriz/friendly-snippets',
+        }
+    },
+    -- Auto-format
+    'sbdchd/neoformat',
+    -- Codecompanion: Alternative AI chat interface
+    {
+        "olimorris/codecompanion.nvim",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "nvim-treesitter/nvim-treesitter",
+            "nvim-telescope/telescope.nvim",
+        },
+        cmd = { "CodeCompanion", "CodeCompanionChat", "CodeCompanionActions" },
+        keys = {
+            { "<leader>ai", "<cmd>CodeCompanionChat Toggle<cr>", desc = "AI Chat" },
+            { "<leader>aa", "<cmd>CodeCompanionActions<cr>",     desc = "AI Actions" },
+            { "<leader>ac", "<cmd>CodeCompanionChat Add<cr>",    mode = "v",         desc = "Add to AI Chat" },
+        },
+        opts = {
+            strategies = {
+                chat = {
+                    adapter = "anthropic",
+                },
+                inline = {
+                    adapter = "anthropic",
+                },
+                agent = {
+                    adapter = "anthropic",
+                },
+            },
+            adapters = {
+                anthropic = function()
+                    return require("codecompanion.adapters").extend("anthropic", {
+                        schema = {
+                            model = {
+                                default = "claude-sonnet-4-20250514",
+                            },
+                        },
+                    })
+                end,
+            },
+            display = {
+                diff = {
+                    provider = "mini_diff",
+                },
+                chat = {
+                    window = {
+                        layout = "vertical",
+                        width = 0.3,
+                    },
+                },
+            },
+        },
+    },
 })
